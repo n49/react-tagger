92d523ee2d126bd7123dfd922a24d78b5456b9ff
Oleg Berman <bermanoleg@gmail.com>
2016-05-11 16:40:55 -0400

    simple mock for plugin

--- example/index.js
+++ example/index.js
@@ -3,9 +3,24 @@ import { render } from 'react-dom'
 
 import Tags from '../src/react-tagger'
 
+let tags = [
+  {
+    id: 1,
+    name: 'Programming'
+  },
+  {
+    id: 2,
+    name: 'Gaming'
+  },
+  {
+    id: 3,
+    name: 'Archery'
+  }
+]
+
 render(
   <div>
-    <Tags />
+    <Tags tags={tags} />
   </div>,
   document.getElementById('root')
 )
--- src/react-tagger.js
+++ src/react-tagger.js
@@ -1,22 +1,69 @@
 import React, { Component, PropTypes } from 'react'
 
+const _inputCSS = {
+  display: 'block',
+  width: '100%',
+  height: '24px',
+  border: '1px solid #ddd',
+  font: '15px Helvetica'
+}
+
+const _tagCSS = {
+  display: 'inline-block',
+  textAlign: 'center',
+  backgroundColor: 'yellow',
+  borderRadius: '4px',
+  marginRight: '5px'
+}
+
+const _tagsWrapperCSS = {
+  display: 'inline-block',
+  position: 'relative',
+  top: '-24px'
+}
+
 class ReactTagger extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      textIndent: 0
+    }
   }
 
   componentDidMount() {
-
+    this.setTextIndent()
   }
 
   componentWillReceiveProps() {
 
   }
 
+  setTextIndent() {
+    this.setState({
+      textIndent: this.refs.tagWrapper.offsetWidth
+    })
+  }
+
+  renderTags() {
+    return this.props.tags.map((tag) => {
+      return <div style={_tagCSS} key={tag.id}>{tag.name}</div>
+    })
+  }
+
   render() {
+
+    let inputCSS = Object.assign({}, _inputCSS, {
+      textIndent: this.state.textIndent
+    })
+
     return (
-      <div><b>It works!</b></div>
+      <div className="react-tagger">
+        <input type="text" style={inputCSS} />
+        <div style={_tagsWrapperCSS} ref="tagWrapper">
+          {this.renderTags()}
+        </div>
+      </div>
     )
   }
 
